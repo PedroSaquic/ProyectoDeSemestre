484746674442f3962340099cfffd08c0d26343d6
Pedro Saquic <psaquicc2@miumg.edu.gt>
2025-09-29 17:15:17 -0600

    se creo projecto SpringBoot

---
+++ bienestar/src/main/java/gt/edu/miumg/bienestar/BienestarApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.miumg.bienestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienestarApplication.class, args);
+	}
+
+}
