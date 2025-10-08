4c6ac30c569d49eca04910219ae1e9e9ce20e2ea
PedroSaquic <psaquicc2@miumg.edu.gt>
2025-10-08 17:11:41 -0600

    agregar dependencias y clases para login seguro

--- bienestar/src/main/java/gt/edu/miumg/bienestar/repository/ClienteRepository.java
+++ bienestar/src/main/java/gt/edu/miumg/bienestar/repository/ClienteRepository.java
@@ -2,9 +2,11 @@
 package gt.edu.miumg.bienestar.repository;
 
 import gt.edu.miumg.bienestar.model.Cliente;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
     boolean existsByCorreo(String correo);
     boolean existsByDpi(String dpi);
+    Optional<Cliente> findByCorreo(String correo);
 }
